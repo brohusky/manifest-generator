d9dc22d983331a7080eac863220e2a9623317508
Max Haze <eng.mahan.hazrati@gmail.com>
2019-08-12 23:18:06 +0700

    add boilerplate

---
+++ start.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const portNumber = process.env.port || 3000;
+const sourceDir = 'dist';
+
+app.use(express.static(sourceDir));
+
+app.listen(portNumber, () => {
+  console.log(`Express web server started: http://localhost:${portNumber}`);
+  console.log(`Serving content from /${sourceDir}/`);
+});
